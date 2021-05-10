571a2dd8ba983d01a077c90a23c149b344375613
Vincenzo <vincenzo.salerno@live.it>
2021-05-11 00:10:59 +0200

    First commit

---
+++ lib/adv_async_widget.dart
@@ -0,0 +1,3 @@
+library adv_async_widget;
+
+export 'package:adv_async_widget/adv_future_builder.dart';
---
+++ lib/adv_future_builder.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AdvFutureBuilder<T> extends StatelessWidget {
+
+	final Future<T> future;
+	final T initialData;
+
+	final Widget Function(BuildContext context) onWait;
+	final Widget Function(BuildContext context, Object error) onError;
+	final Widget Function(BuildContext context, T data) onData;
+
+	const AdvFutureBuilder({
+		Key key,
+		this.future,
+		this.initialData,
+		this.onWait, //TODO se null?
+		this.onError, //TODO se null?
+		this.onData, //TODO se null?
+	}) : super(key: key);
+
+	@override
+	Widget build(BuildContext context) => FutureBuilder<T>(
+		future: future,
+		initialData: initialData,
+		builder: (context, snapshot) {
+			switch(snapshot.connectionState) {
+				//future null case
+				case ConnectionState.none:
+					return onData(context, null);
+				//waiting case
+				case ConnectionState.waiting:
+				case ConnectionState.active:
+					return onWait(context);
+				//done case, running onError or onData depending on future status
+				case ConnectionState.done:
+					return snapshot.hasError? onError(context, snapshot.error) : onData(context, snapshot.data);
+			}
+			//impossible return, only for warning suppression
+			return null;
+		},
+	);
+}
