95c1b50983f8644024ed76016130d6d42119fd33
Vincenzo <vincenzo.salerno@live.it>
2021-05-20 00:33:22 +0200

    add first test on AdvFutureBuilder

--- test/adv_async_widget_test.dart
+++ test/adv_async_widget_test.dart
@@ -1,12 +1,34 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:adv_async_widget/adv_async_widget.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('AdvFutureBuilder without future show onData with data null', (WidgetTester tester) async {
+    // Create the widget
+    await materialAppTest(tester, AdvFutureBuilder(
+        onData: (context, data) => Text(getDataText(data))
+    ));
+
+    // Create the Finder
+    final expectedData = null;
+    final dataFinder = find.text(getDataText(expectedData));
+
+    // verify that the Text widgets appear exactly once in the widget tree
+    expect(dataFinder, findsOneWidget);
   });
+  //TODO: add test to check waiting future
+  //TODO: add test to check future with error
+  //TODO: add test to check future with data
+
+  //TODO: add test to check missing stream
+  //TODO: add test to check waiting stream
+  //TODO: add test to check stream with error
+  //TODO: add test to check stream with data
 }
+
+String getDataText(data) => "data:$data";
+
+Future<void> materialAppTest(tester, widget) => tester.pumpWidget(MaterialApp(
+  home: widget,
+));
